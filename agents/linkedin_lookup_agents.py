d5940a7859bcdbfa50f16b30e01a349a335b0776
Viplav Dube <viplav45@gmail.com>
2025-08-21 00:17:34 +0530

    added one file likedin look up agents

---
+++ agents/linkedin_lookup_agents.py
@@ -0,0 +1,12 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain.agents import (
+    create_react_agent,
+    AgentExecutor,
+)
+from langchain import hub
+from dotenv import load_dotenv
+
+load_dotenv()
